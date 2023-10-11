4202753a250af57552ff040feb9f9de58ee993ea
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2023-10-11 15:53:12 +0530

    Programs till Oct 11th

---
+++ src/sep30th/ArraysDemo3.java
@@ -0,0 +1,37 @@
+package sep30th;
+
+public class ArraysDemo3 {
+
+	public static void main(String[] args) {
+		
+		
+		//syntax 
+		
+		// DataType[] arrayName= new DataType[no of items];
+		
+		int[] a=new int[5];
+		
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+		
+		//a[2]=300;
+		
+		//a[4]=500;
+		
+		a[0]=100;
+		a[1]=200;
+		a[2]=300;
+		a[3]=400;
+		a[4]=500;
+		
+		
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
